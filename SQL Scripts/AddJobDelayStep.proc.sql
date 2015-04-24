ecdc9d7c2eac2cf7838c6d68b9174726c4e0073a
m82labs <mark@m82labs.com>
2015-04-23 22:44:06 -0400

    Added SQL Scripts
    Breaking everything out of the deploy.sql, will then write an extended
    README for deploying all files.

---
+++ SQL Scripts/AddJobDelayStep.proc.sql
@@ -0,0 +1,146 @@
+USE [DBTools]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('DBTools..AddJobDelayStep') IS NULL
+BEGIN
+	EXEC('CREATE PROCEDURE [AddJobDelayStep] AS SELECT 1;');
+END
+GO
+
+----------------------------------------------------------------------------------
+-- Procedure Name: AddJobDelayStep
+--
+-- Desc: Adds a delay step to each job, it can also remove the steps if
+--       @operation = 'D'.
+--
+-- Notes:
+--
+-- Parameters:
+--	INPUT
+--		@operation CHAR(1) - A,D Add or Delete the Delay step from all jobs
+--
+-- Returns:
+--
+-- Date: 2015.02.19 13:44:59
+-- Auth: Mark Wilkinson
+--
+-- Change History
+-- ----------------
+-- Date - Auth: 
+-- Description: 
+----------------------------------------------------------------------------------
+
+ALTER PROCEDURE [AddJobDelayStep]
+(
+@operation CHAR(1) = 'A' -- A - Add, D - Delete
+)
+AS
+
+DECLARE
+    @currentJobID UNIQUEIDENTIFIER,
+    @currentJobName sysname,
+    @sqlCmd NVARCHAR(max);
+ 
+-- Create a cursor for all jobs on the system
+DECLARE JobCursor CURSOR LOCAL FAST_FORWARD
+FOR
+SELECT DISTINCT
+    sj.job_id,
+    sj.name
+FROM
+    msdb.dbo.sysjobservers sjs
+    JOIN msdb.dbo.sysjobs sj
+        ON sj.job_id = sjs.job_id
+    INNER JOIN msdb.dbo.systargetservers AS sts
+        ON sts.server_id = sjs.server_id
+WHERE
+    sts.server_name <> @@servername
+    AND sj.enabled = 1
+    AND sj.originating_server_id = 0
+    AND sj.name <> N'Check replicated subscriber rowcounts';
+
+OPEN JobCursor;
+
+FETCH NEXT FROM JobCursor
+INTO
+    @currentJobID,
+    @CurrentJobName;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    
+    IF (
+        @operation = 'A'
+        AND NOT EXISTS (
+            SELECT  1
+            FROM    msdb.dbo.sysjobsteps
+            WHERE   job_id = @currentJobID
+                    AND step_id = 1
+                    AND step_name = 'Delay'
+        )
+    )
+    BEGIN
+
+        SET @sqlCmd = REPLACE(REPLACE(
+    --<< SQL -----------------------------
+            N'
+            DECLARE @delay INT = NULL;
+            DECLARE @waitfor CHAR(8);
+
+            SELECT  @delay = delay_sec
+            FROM    DBTools.dbo.JobDelay
+            WHERE   job_name = "{{@job_name}}"
+                        
+            SET @waitfor = LEFT(DATEADD(second,ISNULL(@delay,0),CAST("00:00:00" AS TIME)),8);
+
+            WAITFOR DELAY @waitfor;'
+    --<< Subs ----------------------------
+        ,'{{@job_name}}',@currentJobName)
+        ,'"','''');
+
+        EXEC msdb.dbo.sp_add_jobstep @job_id = @currentJobID,
+                                     @step_name = N'Delay',
+                                     @step_id = 1,
+                                     @cmdexec_success_code = 0,
+                                     @on_success_action = 3,
+                                     @on_fail_action = 3,
+                                     @retry_attempts = 0,
+                                     @retry_interval = 0,
+                                     @os_run_priority = 0,
+                                     @subsystem = N'TSQL',
+                                     @command = @sqlCmd,
+                                     @database_name = N'Common',
+                                     @flags = 0;
+
+    END
+
+    IF @operation = 'D'
+    BEGIN
+        IF EXISTS (
+            SELECT  1
+            FROM    msdb.dbo.sysjobsteps
+            WHERE   job_id = @currentJobID
+                    AND step_id = 1
+                    AND step_name = 'Delay'
+        )
+        BEGIN
+            EXEC msdb.dbo.sp_delete_jobstep @job_id = @currentJobID,
+                                            @step_id = 1;
+        END
+    END
+
+    FETCH NEXT FROM JobCursor
+    INTO
+        @currentJobID,
+        @CurrentJobName;
+
+END
+
+CLOSE JobCursor;
+DEALLOCATE JobCursor;
+GO
